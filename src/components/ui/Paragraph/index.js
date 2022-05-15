6bfa84eb0d07d36a79990409322f74e9c4b4be20
Aufa Billah <aufaroot18@gmail.com>
2022-05-15 18:02:29 +0700

    feat: create paragraph component

---
+++ src/components/ui/Paragraph/index.js
@@ -0,0 +1,29 @@
+import { css } from "@emotion/react";
+import styled from "@emotion/styled";
+
+const VARIANTS = {
+  primary: "#06D6A0",
+  secondary: "#118AB2",
+  black: "#073B4C",
+  gray: "#64748B",
+};
+
+const Paragraph = styled.p`
+  margin-top: ${({ mt }) => mt && `${mt}rem`};
+  margin-left: ${({ ml }) => ml && `${ml}rem`};
+  margin-bottom: ${({ mb }) => mb && `${mb}rem`};
+  margin-right: ${({ mr }) => mr && `${mr}rem`};
+  color: ${({ variant }) => variant && VARIANTS[variant]};
+  font-weight: ${({ weight }) => weight && weight};
+  text-align: ${({ align }) => align && align};
+
+  ${({ maxWidth }) =>
+    maxWidth &&
+    css`
+      max-width: ${maxWidth}px;
+      margin-left: auto;
+      margin-right: auto;
+    `}
+`;
+
+export default Paragraph;
