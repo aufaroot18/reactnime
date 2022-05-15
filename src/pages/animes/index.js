d73985aec907b7a93b47ae1dc855520eec6ea2f7
Aufa Billah <aufaroot18@gmail.com>
2022-05-15 18:09:22 +0700

    refactor: update anime page
    add hero and anime list component

--- src/pages/animes/index.js
+++ src/pages/animes/index.js
@@ -1,3 +1,11 @@
-export default function index() {
-  return <h1>List Animes</h1>;
+import AnimeList from "../../components/AnimeList";
+import Hero from "../../components/Hero";
+
+export default function Anime() {
+  return (
+    <>
+      <Hero />
+      <AnimeList />
+    </>
+  );
 }
