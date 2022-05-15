e75aa6adcbd085977c680bb65c69d9e53f900663
Aufa Billah <aufaroot18@gmail.com>
2022-05-15 14:41:40 +0700

    feat: create button component

---
+++ src/components/ui/Button/index.js
@@ -0,0 +1,14 @@
+import styled from "@emotion/styled";
+
+const Button = styled.button`
+  background-color: #06d6a0;
+  color: #fff;
+  border-radius: 50px;
+  padding: 1rem 3rem;
+  cursor: pointer;
+  border: none;
+  font-size: 1rem;
+  font-weight: 600;
+`;
+
+export default Button;
