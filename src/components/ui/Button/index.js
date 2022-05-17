b0345b5f27cb56fd1b10dbaac672a90c749039c0
Aufa Billah <aufaroot18@gmail.com>
2022-05-17 17:06:10 +0700

    refactor: update button component
    add margin

--- src/components/ui/Button/index.js
+++ src/components/ui/Button/index.js
@@ -1,14 +1,25 @@
 import styled from "@emotion/styled";
 
+const VARIANTS = {
+  primary: "#06D6A0",
+  secondary: "#118AB2",
+  danger: "#EF476F",
+};
+
 const Button = styled.button`
-  background-color: #06d6a0;
+  text-decoration: none;
+  background-color: ${({ variant }) => VARIANTS[variant] || VARIANTS.primary};
   color: #fff;
-  border-radius: 50px;
-  padding: 1rem 3rem;
+  border-radius: 5px;
+  padding: 0.5rem 0.7rem;
   cursor: pointer;
   border: none;
   font-size: 1rem;
   font-weight: 600;
+  margin-top: ${({ mt }) => mt && `${mt}rem`};
+  margin-right: ${({ mr }) => mr && `${mr}rem`};
+  margin-bottom: ${({ mb }) => mb && `${mb}rem`};
+  margin-left: ${({ ml }) => ml && `${ml}rem`};
 `;
 
 export default Button;
