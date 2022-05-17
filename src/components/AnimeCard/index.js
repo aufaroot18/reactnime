5630ac1dd3af2b67325bd113f165897fceff359c
Aufa Billah <aufaroot18@gmail.com>
2022-05-17 17:19:26 +0700

    refactor: update animecard and animelist

--- src/components/AnimeCard/index.js
+++ src/components/AnimeCard/index.js
@@ -1,12 +1,46 @@
 import styled from "@emotion/styled";
+import Link from "../ui/Link";
+import Heading from "../ui/Heading";
+import Image from "../ui/Image";
 
-const AnimeCard = styled.div`
+const StyledAnimeCard = styled.div`
   text-align: center;
   flex-basis: 50%;
   margin-bottom: 2rem;
   max-width: 200px;
   margin-left: auto;
   margin-right: auto;
+
+  h3 {
+    font-size: 1.3rem;
+  }
+
+  h4 {
+    font-size: 1.1rem;
+  }
+
+  img {
+    margin-left: auto;
+    margin-right: auto;
+    height: 300px;
+    margin-bottom: 1rem;
+    border-radius: 10px;
+    box-shadow: rgb(100 100 111 / 20%) 0px 7px 29px 0px;
+  }
 `;
 
-export default AnimeCard;
+export default function AnimeCard({ anime }) {
+  return (
+    <StyledAnimeCard>
+      <Image src={anime.coverImage.large} alt="anime" />
+      <Heading as="h3" mb="0.5" weight="500">
+        <Link to={`/anime/${anime.id}`} variant="primary">
+          {anime.title.english}
+        </Link>
+      </Heading>
+      <Heading as="h4" weight="500" variant="gray">
+        {anime.seasonYear || "N/A"}
+      </Heading>
+    </StyledAnimeCard>
+  );
+}
--- src/components/AnimeList/index.js
+++ src/components/AnimeList/index.js
@@ -1,68 +1,68 @@
 import { useQuery } from "@apollo/client";
-import styled from "@emotion/styled";
 import { GET_ANIME } from "../../graphql/queries";
 import AnimeCard from "../AnimeCard";
 import Heading from "../ui/Heading";
 import Loading from "../ui/Loading";
 import Paragraph from "../ui/Paragraph";
 import Section from "../ui/Section";
-
-const AnimeImage = styled.img`
-  margin-left: auto;
-  margin-right: auto;
-  height: 300px;
-  margin-bottom: 1rem;
-  border-radius: 10px;
-  box-shadow: rgb(100 100 111 / 20%) 0px 7px 29px 0px;
-`;
-
-const AnimeWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  flex-wrap: wrap;
-  justify-content: center;
-
-  @media screen and (min-width: 768px) {
-    flex-direction: row;
-  }
-`;
-
-const Pagination = styled.nav``;
+import Flex from "../ui/Flex";
+import { useState } from "react";
+import Pagination, { PaginationItem } from "../ui/Pagination";
+import Box from "../ui/Box";
 
 export default function AnimeList() {
-  const { loading, error, data } = useQuery(GET_ANIME);
+  const [currentPage, setCurrentPage] = useState(1);
+  const queryVariables = {
+    variables: {
+      page: currentPage,
+    },
+  };
+  const { loading, error, data } = useQuery(GET_ANIME, queryVariables);
 
-  if (error) return "Something error";
+  if (loading) return <Loading />;
+  if (error)
+    return (
+      <Heading variant="primary" align="center">
+        Something Error
+      </Heading>
+    );
 
-  const animeCards =
-    data &&
-    data.Page.media.map((anime) => {
-      return (
-        <AnimeCard key={anime.id}>
-          <AnimeImage src={anime.coverImage.large} alt="anime" />
-          <Heading as="h3" weight="500" variant="gray" mb="1" fontSize="1.5">
-            {anime.title.english}
-          </Heading>
-          <Paragraph weight="500">{anime.seasonYear || "N/A"}</Paragraph>
-        </AnimeCard>
-      );
-    });
+  const animes = data.Page.media;
+  const page = data.Page.pageInfo;
+
+  function nextPage() {
+    page.hasNextPage && setCurrentPage((prevState) => prevState + 1);
+  }
+
+  function prevPage() {
+    currentPage > 1 && setCurrentPage((prevState) => prevState - 1);
+  }
 
   return (
-    <Section>
+    <Section mb="3" mt="3">
       <Heading mb="1" variant="primary" align="center">
-        Trending Anime
+        Top Anime
       </Heading>
       <Paragraph mb="3" align="center" maxWidth="600" variant="gray">
         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab inventore
         dolores, dolore est saepe explicabo eum similique consequuntur vitae et?
       </Paragraph>
-      <AnimeWrapper>{loading ? <Loading /> : animeCards}</AnimeWrapper>
-      <Pagination>
-        <Heading as="h2" align="center">
-          Pagination Here
-        </Heading>
-      </Pagination>
+      <Flex>
+        {animes.map((anime) => (
+          <AnimeCard key={anime.id} anime={anime} />
+        ))}
+      </Flex>
+      <Box mt="3">
+        <Pagination>
+          <PaginationItem onClick={prevPage} link>
+            {currentPage > 1 && "Prev"}
+          </PaginationItem>
+          <PaginationItem>{page.currentPage}</PaginationItem>
+          <PaginationItem onClick={nextPage} link>
+            {page.hasNextPage && "Next"}
+          </PaginationItem>
+        </Pagination>
+      </Box>
     </Section>
   );
 }
