3c270ba2b6554661820f165ad72e6773b8e136cf
Aufa Billah <aufaroot18@gmail.com>
2022-05-17 17:07:07 +0700

    refactor: add margin to section component

--- src/components/ui/Section/index.js
+++ src/components/ui/Section/index.js
@@ -1,5 +1,10 @@
 import styled from "@emotion/styled";
 
-const Section = styled.section``;
+const Section = styled.section`
+  margin-top: ${({ mt }) => mt && `${mt}rem`};
+  margin-left: ${({ ml }) => ml && `${ml}rem`};
+  margin-bottom: ${({ mb }) => mb && `${mb}rem`};
+  margin-right: ${({ mr }) => mr && `${mr}rem`};
+`;
 
 export default Section;
