beaedf70bea7f291652ea59f739f4c4869084337
Aufa Billah <aufaroot18@gmail.com>
2022-05-18 11:15:41 +0700

    add box-shadow to modal

--- src/components/ui/Forms/index.js
+++ src/components/ui/Forms/index.js
@@ -1,4 +1,10 @@
+import styled from "@emotion/styled";
 import Input from "./Input";
 import Label from "./Label";
 
+const Form = styled.form`
+  width: 100%;
+`;
+
+export default Form;
 export { Input, Label };
--- src/components/ui/Modal/index.js
+++ src/components/ui/Modal/index.js
@@ -3,19 +3,15 @@ import ReactModal from "react-modal";
 
 const Modal = styled(ReactModal)`
   position: absolute;
-  border: 1px solid rgb(204, 204, 204);
   background: rgb(255, 255, 255);
-  overflow: auto;
-  border-radius: 4px;
-  outline: none;
   padding: 1rem;
+  overflow: auto;
+  border-radius: 10px;
   top: 20%;
   left: 50%;
-  right: auto;
-  bottom: auto;
-  margin-right: -50%;
   transform: translate(-50%, -20%);
   text-transform: capitalize;
+  box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
 `;
 
 export default Modal;
