15a06d29beecb6814426faf95f4da5bd63594bc2
Aufa Billah <aufaroot18@gmail.com>
2022-05-18 11:15:53 +0700

    refactor: add validation in form

--- src/components/AddCollectionModal/index.js
+++ src/components/AddCollectionModal/index.js
@@ -2,9 +2,11 @@ import { nanoid } from "nanoid";
 import { useState } from "react";
 import Box from "../ui/Box";
 import Button from "../ui/Button";
-import { Input, Label } from "../ui/Forms";
+import Form, { Input, Label } from "../ui/Forms";
 import Heading from "../ui/Heading";
 import placeholder from "../../assets/img/placeholder.jpg";
+import { setItemLocalStorage } from "../../utils/helpers/localstorage";
+import Paragraph from "../ui/Paragraph";
 
 export default function AddCollectionModal({
   anime,
@@ -12,18 +14,38 @@ export default function AddCollectionModal({
   collections,
 }) {
   const [name, setName] = useState("");
+  const [nameError, setNameError] = useState(false);
 
+  /**
+   * HANDLE INPUT FORM
+   * @param {object} e - event object
+   */
   function handleChange(e) {
     setName(e.target.value);
   }
 
+  /**
+   * HANDLE FORM IF SUBMITTED
+   * @param {object} e - event object
+   */
   function handleSubmit(e) {
     e.preventDefault();
+    validation() && addAnimeToCollection();
+  }
 
+  /**
+   * ADD ANIME TO COLLECTION
+   */
+  function addAnimeToCollection() {
+    // CHECK IS COLLECTION EXIST
     const isCollectoinExist = collections.find(
       (collection) => name.toLowerCase() === collection.name.toLowerCase()
     );
 
+    /**
+     * IF EXIST, PUSH ANIME TO COLLECTION
+     * IF NO, CREATE NEW COLLECTIOn
+     */
     if (isCollectoinExist) {
       anime && isCollectoinExist.anime.push(anime);
     } else {
@@ -35,10 +57,24 @@ export default function AddCollectionModal({
       });
     }
 
-    localStorage.setItem("collections", JSON.stringify(collections));
-    toggleModal();
+    // SAVE COLLECTIONS TO LOCALSTORAGE
+    setItemLocalStorage("collections", collections);
 
+    toggleModal();
     setName("");
+  }
+
+  /**
+   * CHECK VALIDATION FORM
+   */
+  function validation() {
+    console.log("validation");
+    if (name === "") {
+      console.log("kosong");
+      setNameError(true);
+      return false;
+    }
+    return true;
   }
 
   return (
@@ -46,16 +82,26 @@ export default function AddCollectionModal({
       <Heading variant="primary" mb="1" align="center">
         Add Collection
       </Heading>
-      <form onSubmit={handleSubmit}>
+      <Form onSubmit={handleSubmit}>
         <Box>
           <Label htmlFor="name">Collection Name</Label>
-          <Input id="name" type="text" value={name} onChange={handleChange} />
+          <Input
+            id="name"
+            type="text"
+            value={name}
+            onChange={handleChange}
+            autoFocus
+            required
+          />
         </Box>
-        <Button mr="0.5">Add</Button>
-        <Button onClick={toggleModal} type="button">
-          Close
-        </Button>
-      </form>
+        {nameError && <Paragraph variant="danger">Name is required</Paragraph>}
+        <Box mt="1">
+          <Button mr="0.5">Add</Button>
+          <Button onClick={toggleModal} type="button">
+            Close
+          </Button>
+        </Box>
+      </Form>
     </Box>
   );
 }
